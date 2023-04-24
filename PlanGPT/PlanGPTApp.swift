9b53c88532d3acb77da232dbf59c03e1a7d5db6e
David Yang <davidkunheeyang@gmail.com>
2023-04-24 15:23:47 -0700

    Initial Commit

---
+++ PlanGPT/PlanGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanGPTApp.swift
+//  PlanGPT
+//
+//  Created by David Yang on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
